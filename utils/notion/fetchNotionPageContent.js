de105c51d5ddea206b03a0413ac800486cee2c8e
flashsoon <developsoon@gmail.com>
2024-11-25 22:40:57 +0900

    add pagination to fetch blocks over 100

--- utils/notion/fetchNotionPageContent.js
+++ utils/notion/fetchNotionPageContent.js
@@ -2,13 +2,27 @@ const { notion } = require("./client")
 
 const fetchNotionPageContent = async pageId => {
   try {
-    const response = await notion.blocks.children.list({
-      block_id: pageId,
-    })
-    return response.results
+    let allBlocks = []
+    let hasMore = true
+    let startCursor
+
+    while (hasMore) {
+      const response = await notion.blocks.children.list({
+        block_id: pageId,
+        start_cursor: startCursor,
+        page_size: 100, // 한 번에 가져올 최대 블록 수
+      })
+
+      allBlocks = [...allBlocks, ...response.results]
+      hasMore = response.has_more
+      startCursor = response.next_cursor
+    }
+
+    return allBlocks
   } catch (error) {
     console.error("Error fetching Notion page content:", error)
     return []
   }
 }
+
 module.exports = { fetchNotionPageContent }
